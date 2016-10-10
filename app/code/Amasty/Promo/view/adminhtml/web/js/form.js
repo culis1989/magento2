a4d80828c66086947420cb33d03e1711a86f6ab2
Filip Kunjadić - Ćulibrk <filip.kunjadic@pixel2go.com>
2016-10-10 13:33:34 +0200

    amasty promo module added

---
+++ app/code/Amasty/Promo/view/adminhtml/web/js/form.js
@@ -0,0 +1,81 @@
+define([
+    'jquery',
+    'uiRegistry'
+], function ($, registry) {
+    var fieldset_prefix = 'sales_rule_form.sales_rule_form.actions.';
+
+    var ampromoForm = {
+        update: function () {
+            this.resetFields();
+
+            var actionFieldset = $('#sales_rule_formrule_actions_fieldset_').parent();
+
+            window.amPromoHide = 0;
+            actionFieldset.show();
+            if (typeof window.amRulesHide !="undefined" && window.amRulesHide == 1) {
+                actionFieldset.hide();
+            }
+            
+
+            var action = $('[data-index="simple_action"] select').val();
+
+            if (action.match(/^ampromo/)) {
+                this.hideFields(['simple_free_shipping', 'apply_to_shipping']);
+            }
+
+            switch (action) {
+                case 'ampromo_cart':
+                    actionFieldset.hide();
+                    window.amPromoHide = 1;
+
+                    this.hideFields(['discount_qty', 'discount_step']);
+                    this.showFields(['ampromorule[sku]', 'ampromorule[type]']);
+                    break;
+                case 'ampromo_items':
+                    this.showFields(['ampromorule[sku]', 'ampromorule[type]']);
+                    break;
+                case 'ampromo_product':
+                    break;
+                case 'ampromo_spent':
+                    actionFieldset.hide();
+                    window.amPromoHide = 1;
+
+                    this.showFields(['ampromorule[sku]', 'ampromorule[type]']);
+                    break;
+            }
+        },
+
+        resetFields: function () {
+            this.showFields([
+                'discount_qty', 'discount_step', 'apply_to_shipping', 'simple_free_shipping'
+            ]);
+            this.hideFields(['ampromorule[sku]', 'ampromorule[type]']);
+        },
+
+        hideFields: function (names) {
+            return this.toggleFields('hide', names);
+        },
+
+        showFields: function (names) {
+            return this.toggleFields('show', names);
+        },
+
+        addPrefix: function (names) {
+            for (var i = 0; i < names.length; i++) {
+                names[i] = fieldset_prefix + names[i];
+            }
+
+            return names;
+        },
+
+        toggleFields: function (method, names) {
+            registry.get(this.addPrefix(names), function () {
+                for (var i = 0; i < arguments.length; i++) {
+                    arguments[i][method]();
+                }
+            });
+        }
+    };
+
+    return ampromoForm;
+});
